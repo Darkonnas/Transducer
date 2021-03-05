204df6f482ccb76110333266c8045c053a0add85
Darkonnas <vlad.stoleru@gmail.com>
2021-03-05 17:41:32 +0200

    Added the option to chose between which transducer to use

--- main.cpp
+++ main.cpp
@@ -5,34 +5,88 @@
 #include <vector>
 
 int main() {
-    std::ifstream input("../Configurations/StackTransducer1.txt");
-    stackTransducer ST(input);
-    input.close();
 
-    ST.printConfiguration();
+    bool invalidChoice;
+    size_t choice;
+    do {
+        invalidChoice = false;
 
-    size_t count;
+        std::cout << "Please choose one of the available transducers:\n";
+        std::cout << "1. Finite State Transducer\n";
+        std::cout << "2. Stack Transducer\n";
+        std::cout << "\nYour choice:";
+        std::cin >> choice;
 
-    std::cout << "\nHow many words would you like to translate?\ncount =";
-    std::cin >> count;
+        switch (choice) {
+            case 1: {
+                std::ifstream fstInput("../Configurations/FiniteTransducer2.txt");
+                finiteStateTransducer fst(fstInput);
+                fstInput.close();
 
-    std::vector<std::string> words(count);
+                fst.printConfiguration();
 
-    std::cout << "Please input the words:\n";
+                size_t count;
 
-    for (size_t i = 0; i < count; ++i) {
-        std::cout << i + 1 << ".";
-        std::cin >> words[i];
-    }
+                std::cout << "\nHow many words would you like to translate?\ncount =";
+                std::cin >> count;
 
-    std::cout << "\nResult:\n";
+                std::vector<std::string> words(count);
 
-    for (const auto& word : words) {
-        std::cout << word << " can be translated into: ";
-        for (const auto& translation : ST.translate(word)) {
-            std::cout << "\"" << translation << "\" ";
+                std::cout << "Please input the words:\n";
+
+                for (size_t i = 0; i < count; ++i) {
+                    std::cout << i + 1 << ".";
+                    std::cin >> words[i];
+                }
+
+                std::cout << "\nResult:\n";
+
+                for (const auto &word : words) {
+                    std::cout << word << " can be translated into: ";
+                    for (const auto &translation : fst.translate(word)) {
+                        std::cout << "\"" << translation << "\" ";
+                    }
+                    std::cout << '\n';
+                }
+                break;
+            }
+            case 2: {
+                std::ifstream sttInput("../Configurations/StackTransducer2.txt");
+                stackTransducer stt(sttInput);
+                sttInput.close();
+
+                stt.printConfiguration();
+
+                size_t count;
+
+                std::cout << "\nHow many words would you like to translate?\ncount =";
+                std::cin >> count;
+
+                std::vector<std::string> words(count);
+
+                std::cout << "Please input the words:\n";
+
+                for (size_t i = 0; i < count; ++i) {
+                    std::cout << i + 1 << ".";
+                    std::cin >> words[i];
+                }
+
+                std::cout << "\nResult:\n";
+
+                for (const auto &word : words) {
+                    std::cout << word << " can be translated into: ";
+                    for (const auto &translation : stt.translate(word)) {
+                        std::cout << "\"" << translation << "\" ";
+                    }
+                    std::cout << '\n';
+                }
+                break;
+            }
+            default: {
+                std::cout << "\nInvalid choice!\n\n";
+                invalidChoice = true;
+            }
         }
-        std::cout << '\n';
-    }
+    } while (invalidChoice);
     return 0;
 }
